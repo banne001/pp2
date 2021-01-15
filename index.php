0488ddc89d395fd175dcaf6de9df1ac63bb3576e
Blezyl Santos <blezylsantos@yahoo.com>
2021-01-15 15:15:48 -0500

    Added index.php and header for the file. Finish Step One on Assignment

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Pair Program 2
+ * name: Blezyl Santos and Safal A
+ * Date: 1/15/2021
+ * git: https://github.com/banne001/pp2.git
+ * url: http://blezylsantos.greenriverdev.com/328/pp2/
+ */
+// Error Reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+?>
+
+<?php
+echo "PHP Array Practice";
+
+
+?>
